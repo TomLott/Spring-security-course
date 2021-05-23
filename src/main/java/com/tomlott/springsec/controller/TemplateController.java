9f21214785143f9a5d82a9dca3c68cdd489929dd
India Tollett <itollett@et-k4.kzn.21-school.ru>
2021-05-23 16:33:50 +0300

    Login/logout pages

--- src/main/java/com/tomlott/springsec/controller/TemplateController.java
+++ src/main/java/com/tomlott/springsec/controller/TemplateController.java
@@ -3,6 +3,7 @@ package com.tomlott.springsec.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -18,4 +19,6 @@ public class TemplateController {
     public String courses(){
         return "courses";
     }
+
+
 }
