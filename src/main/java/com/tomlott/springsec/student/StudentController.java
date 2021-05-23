0206c3fb30094909d435016a6375f20db1a7c902
India Tollett <itollett@et-k4.kzn.21-school.ru>
2021-05-23 15:30:56 +0300

    first commit

---
+++ src/main/java/com/tomlott/springsec/student/StudentController.java
@@ -0,0 +1,30 @@
+package com.tomlott.springsec.student;
+
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/students")
+public class StudentController {
+
+    private static final List<Student> STUDENTS = Arrays.asList(
+            new Student(1, "James Bond"),
+            new Student(2, "Maria Jons"),
+            new Student(3, "Anna Smith")
+    );
+
+//    @PreAuthorize("hasAuthority('student:read')")
+    @GetMapping("{studentId}")
+    public Student getStudent(@PathVariable("studentId") Integer studentId){
+        return STUDENTS.stream().filter(a->a.getStudentId().equals(studentId)).findFirst().orElseThrow(()->new IllegalStateException("Student + " + studentId + " doesn't exist"));
+    }
+
+}
+
